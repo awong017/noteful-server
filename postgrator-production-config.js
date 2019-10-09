967850650fe5aa803858f089ea37d45ec4a5161d
Adam Wong <awong017@ucr.edu>
2019-10-08 20:39:17 -0700

    added connection string

--- postgrator-production-config.js
+++ postgrator-production-config.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 
 module.exports = { 
     "migrationDirectory": "migrations", 
-    "driver": "pg", 
+    "driver": "pg",
+    "connectionString": (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
     "host": process.env.PROD_MIGRATION_DB_HOST, 
     "port": process.env.PROD_MIGRATION_DB_PORT, 
     "database": process.env.PROD_MIGRATION_DB_NAME, 
