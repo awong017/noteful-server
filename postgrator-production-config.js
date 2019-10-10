e90af9763e9ee28820f9b54273112e308d443c53
Adam Wong <awong017@ucr.edu>
2019-10-10 16:56:20 -0700

    fixed migrate:production script in package json

--- postgrator-production-config.js
+++ postgrator-production-config.js
@@ -3,13 +3,10 @@ require('dotenv').config();
 module.exports = { 
     "migrationDirectory": "migrations", 
     "driver": "pg",
-    "connectionString": (process.env.NODE_ENV === 'test')
-    ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL,
     "host": process.env.PROD_MIGRATION_DB_HOST, 
     "port": process.env.PROD_MIGRATION_DB_PORT, 
     "database": process.env.PROD_MIGRATION_DB_NAME, 
     "username": process.env.PROD_MIGRATION_DB_USER, 
     "password": process.env.PROD_MIGRATION_DB_PASS, 
-    "ssl": false
+    "ssl": true
  }
